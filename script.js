e4ff787ec62589477f2d8f44f73e386ed54848c1
Asisipho <sipho.msitho@gmail.com>
2021-01-11 12:50:17 +0200

    Edited index.html and script.js

--- script.js
+++ script.js
@@ -1,58 +1,78 @@
+//Displaying quantity of selected items on the counter
+function quantityFunction() {
+  document.getElementById("counter").innerHTML = document.getElementById("quantity").value;
+  document.getElementById("color-quantity").innerHTML = document.getElementById("quantity").value;
+  num1 = document.getElementById("quantity").value;
+  num2 = document.getElementById("discount-price").innerHTML;
+  document.getElementById("full-price").innerHTML = num1 * num2;
+  document.getElementById("order-info").innerHTML = document.getElementByClass("color-circles").button;
+}
+
+
 // Display color names on the modal and color title; display prices
 function colorFunction() {
   document.getElementById("color-name").innerHTML = " Red";
-  document.getElementById("actual-price").innerHTML =" R30.99";
-  document.getElementById("discount-price").innerHTML =" R23.35";
+  document.getElementById("actual-price").innerHTML =" 30.99";
+  document.getElementById("discount-price").innerHTML =" 23.35";
   document.getElementById("color-id").innerHTML =" Red"
+  document.getElementById("color-price").innerHTML = " 23.35";
+  
 }
 
 function colorFunction2() {
   document.getElementById("color-name").innerHTML = " Blue";
-  document.getElementById("actual-price").innerHTML =" R32.99";
-  document.getElementById("discount-price").innerHTML =" R24.75";
+  document.getElementById("actual-price").innerHTML =" 32.99";
+  document.getElementById("discount-price").innerHTML =" 24.75";
   document.getElementById("color-id").innerHTML =" Blue"
+  document.getElementById("color-price").innerHTML = " 24.75";
 }
 
 function colorFunction3() {
   document.getElementById("color-name").innerHTML = " Orange";
-  document.getElementById("actual-price").innerHTML =" R35.99";
-  document.getElementById("discount-price").innerHTML =" R26.99";
+  document.getElementById("actual-price").innerHTML =" 35.99";
+  document.getElementById("discount-price").innerHTML =" 26.99";
   document.getElementById("color-id").innerHTML =" Orange"
+  document.getElementById("color-price").innerHTML = " 26.99";
 }
 
 function colorFunction4() {
   document.getElementById("color-name").innerHTML = " Green";
-  document.getElementById("actual-price").innerHTML =" R24.99";
-  document.getElementById("discount-price").innerHTML =" R18.75";
-  document.getElementById("color-id").innerHTML =" Green"
+  document.getElementById("actual-price").innerHTML =" 24.99";
+  document.getElementById("discount-price").innerHTML =" 18.75";
+  document.getElementById("color-id").innerHTML =" Green";
+  document.getElementById("color-price").innerHTML = " 18.75";
 }
 
 function colorFunction5() {
   document.getElementById("color-name").innerHTML = " Dark-grey";
-  document.getElementById("actual-price").innerHTML =" R37.99";
-  document.getElementById("discount-price").innerHTML =" R28.5";
-  document.getElementById("color-id").innerHTML =" Dark-grey"
+  document.getElementById("actual-price").innerHTML =" 37.99";
+  document.getElementById("discount-price").innerHTML =" 28.5";
+  document.getElementById("color-id").innerHTML =" Dark-grey";
+  document.getElementById("color-price").innerHTML = " 28.5";
 }
 
 function colorFunction6() {
   document.getElementById("color-name").innerHTML = " Teal";
-  document.getElementById("actual-price").innerHTML =" R44.99";
-  document.getElementById("discount-price").innerHTML =" R33.75";
-  document.getElementById("color-id").innerHTML =" Teal"
+  document.getElementById("actual-price").innerHTML =" 44.99";
+  document.getElementById("discount-price").innerHTML =" 33.75";
+  document.getElementById("color-id").innerHTML =" Teal";
+  document.getElementById("color-price").innerHTML = " 33.75";
 }
 
 function colorFunction7() {
   document.getElementById("color-name").innerHTML = " Grey";
-  document.getElementById("actual-price").innerHTML =" R31.99";
-  document.getElementById("discount-price").innerHTML =" R23.99";
-  document.getElementById("color-id").innerHTML =" Blue"
+  document.getElementById("actual-price").innerHTML =" 31.99";
+  document.getElementById("discount-price").innerHTML =" 23.99";
+  document.getElementById("color-id").innerHTML =" Blue";
+  document.getElementById("color-price").innerHTML = " 23.99";
 }
 
 function colorFunction8() {
   document.getElementById("color-name").innerHTML = " Light-grey";
-  document.getElementById("actual-price").innerHTML =" R33.99";
-  document.getElementById("discount-price").innerHTML =" R25.5";
-  document.getElementById("color-id").innerHTML =" Light-grey"
+  document.getElementById("actual-price").innerHTML =" 33.99";
+  document.getElementById("discount-price").innerHTML =" 25.5";
+  document.getElementById("color-id").innerHTML =" Light-grey";
+  document.getElementById("color-price").innerHTML = " 25.5";
 }
 // Get the modal
 var modal = document.getElementById("myModal");
@@ -122,10 +142,6 @@ document.querySelector('#subtract').addEventListener('click', function()
   document.getElementById('quantity').value = valueCount
 })
 
-
-function quantityFunction() {
-  document.getElementById("counter").innerHTML = document.getElementById("quantity").value;
-}
 
 
 // Get the modal
